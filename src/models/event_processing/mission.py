e86f7b23cbae33d2c572e65c778a4b1acb932b10
Erick <edcm.erick@gmail.com>
2023-12-06 18:53:22 -0500

    calculate duration of a mission in days

--- src/models/event_processing/mission.py
+++ src/models/event_processing/mission.py
@@ -2,17 +2,18 @@ from src.models.event_processing.device import Device
 from typing import List
 from pydantic import BaseModel, computed_field
 import yaml
+from datetime import datetime
 
 class Mission(BaseModel):
-    start_date: str
-    end_date: str
+    start_date: datetime
+    end_date: datetime
     budget: float
     devices: List[Device]
 
     @computed_field
     @property
-    def duration(self)->str:
-        return ''.join([self.end_date, self.start_date])
+    def duration(self)->int:
+        return (self.end_date - self.start_date).days
 
     def generate_event(self, name:str) -> None:
         with open(name, 'w+') as file:
--- src/test.py
+++ src/test.py
@@ -2,13 +2,15 @@ from src.models.event_processing.orbit_one import OrbitOne
 from src.models.event_processing.service_type import ServiceType
 from src.models.event_processing.device  import Device
 from src.models.event_processing.device_status import DeviceStatus
+from datetime import datetime
 
-orbit_one = OrbitOne(start_date='d', 
-    end_date = 'a',
+orbit_one = OrbitOne(
+    start_date=datetime.now(), 
+    end_date = datetime(2024, 2, 26),
     budget= 2,
-    devices= [Device(id=2, device_status=DeviceStatus.EXCELLENT, device_type='d', device_age=2)],
     satellite_name='OpenAI',
-    service_type= ServiceType.UPDATE
-    )
+    service_type= ServiceType.UPDATE,
+    devices= [Device(id=2, device_status=DeviceStatus.EXCELLENT, device_type='d', device_age=2)],
+)
 
 orbit_one.generate_event()
