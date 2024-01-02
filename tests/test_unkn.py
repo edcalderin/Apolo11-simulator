03217525574fe1cf0f51fdbca978b5de43e566e9
PabloAlarcon1 <apablo.alarcon@gmail.com>
2024-01-01 23:17:13 -0500

    Include test test_unkn.py

--- tests/test_unkn.py
+++ tests/test_unkn.py
@@ -1,10 +1,14 @@
 import pytest
+import uuid  # Importa el módulo uuid
 from datetime import datetime
 from src.models.event_processing.unkn import Unkn
 from src.models.event_processing.device import Device
 from src.models.event_processing.device_status import DeviceStatus
 
 def test_generate_event_for_unkn():
+    # Genera un UUID único para la instancia de Unkn
+    unique_uuid = str(uuid.uuid4())
+    
     # Definir una lista de dispositivos con todos los campos requeridos
     devices = [
         Device(id=1, device_type="TypeA", device_status=DeviceStatus.EXCELLENT, device_age=2),
@@ -15,13 +19,12 @@ def test_generate_event_for_unkn():
     start_date = datetime(2023, 1, 1)
     end_date = datetime(2023, 1, 10)
     budget = 1000.0
-    unkn_instance = Unkn(start_date=start_date, end_date=end_date, budget=budget, devices=devices)
+    unkn_instance = Unkn(start_date=start_date, end_date=end_date, budget=budget, devices=devices, uuid=unique_uuid)  # Añade el uuid generado
     
     # Generar el evento para la instancia Unkn
     unkn_instance.generate_event()
     
-    # Aquí podrías agregar aserciones adicionales si deseas verificar algo específico después de generar el evento.
-
+    
 # Ejecutar las pruebas con pytest
 if __name__ == "__main__":
     pytest.main([__file__])
