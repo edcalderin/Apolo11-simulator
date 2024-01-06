159eefc3d16915d898ce787d154fc5512ec57982
edcalderin <edcm.erick@gmail.com>
2024-01-05 19:58:22 -0500

    hash method to mission class, device a single object instead of a list

--- src/models/event_processing/device.py
+++ src/models/event_processing/device.py
@@ -1,20 +1,10 @@
 from src.models.event_processing.device_status import DeviceStatus
 from pydantic import ConfigDict
-import hashlib
 from pydantic import BaseModel
 
 class Device(BaseModel):
     model_config = ConfigDict(use_enum_values=True)
-    id: int
     device_type: str
-    device_status: DeviceStatus
-    device_age: int
-
-    def get_hash(self, mission_date: str, mission_name: str) -> str:
-        hash = hashlib.sha256()
-        hash.update(mission_date.encode())
-        hash.update(mission_name.encode())
-        hash.update(self.device_type.encode())
-        hash.update(self.device_status.encode())
-        return hash.hexdigest()
-
+    device_status: DeviceStatus | str
+    device_age: int | str
+    
--- src/models/event_processing/mission.py
+++ src/models/event_processing/mission.py
@@ -1,23 +1,29 @@
 from src.models.event_processing.device import Device
-from typing import List
 from pydantic import BaseModel, computed_field
 import yaml
 from datetime import datetime
+import hashlib
 
 class Mission(BaseModel):
-    start_date: datetime
-    end_date: datetime
-    budget: float
-    devices: List[Device]
-
+    date: datetime
+    budget: float | str
+    device: Device
+    
     @computed_field
     @property
-    def duration(self)->int:
-        return (self.end_date - self.start_date).days
+    def mission(self)-> str:
+        return self.__class__.__name__
+    
+    @computed_field
+    @property
+    def hash(self) -> str:
+        hash = hashlib.sha256()
+        hash.update(str(self.date).encode())
+        hash.update(self.mission.encode())
+        hash.update(self.device.device_type.encode())
+        hash.update(self.device.device_status.encode())
+        return hash.hexdigest()
 
     def generate_event(self, name: str) -> None:
-        model_dict = self.model_dump()
-        for device in model_dict.get('devices'):
-            device['hash'] = Device(**device).get_hash(str(self.start_date), self.__class__.__name__)
         with open(name, 'w+') as file:
-            yaml.dump(model_dict, file)
+            yaml.dump(self.model_dump(), file)
