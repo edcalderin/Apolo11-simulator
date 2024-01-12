f2614c98b2049152487e14bc08b79ff570b97ab0
PabloAlarcon1 <apablo.alarcon@gmail.com>
2024-01-11 20:19:00 -0500

    UUID included in mission

--- tests/test_unkn.py
+++ tests/test_unkn.py
@@ -6,20 +6,14 @@ from src.models.event_processing.device import Device
 from src.models.event_processing.device_status import DeviceStatus
 
 def test_generate_event_for_unkn():
-    # Genera un UUID único para la instancia de Unkn
-    unique_uuid = str(uuid.uuid4())
-    
+        
     # Definir una lista de dispositivos con todos los campos requeridos
-    devices = [
-        Device(id=1, device_type="TypeA", device_status=DeviceStatus.EXCELLENT, device_age=2),
-        Device(id=2, device_type="TypeB", device_status=DeviceStatus.GOOD, device_age=3)
-    ]
+    device = Device(device_type="TypeA", device_status=DeviceStatus.EXCELLENT, device_age=2)
+    
     
     # Crear una instancia de Unkn con los parámetros definidos
-    start_date = datetime(2023, 1, 1)
-    end_date = datetime(2023, 1, 10)
-    budget = 1000.0
-    unkn_instance = Unkn(start_date=start_date, end_date=end_date, budget=budget, devices=devices, uuid=unique_uuid)  # Añade el uuid generado
+    date = datetime(2023, 1, 1)    
+    unkn_instance = Unkn(date=date, budget="unknown", device=device) 
     
     # Generar el evento para la instancia Unkn
     unkn_instance.generate_event()
