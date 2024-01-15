cc1a18f0322031552f332f7de03f26bec3117e2d
Aarteagaj <anabelaj91@gmail.com>
2024-01-15 17:26:57 -0500

    Se agrega documentacion

--- src/utils.py
+++ src/utils.py
@@ -5,5 +5,13 @@ class Utils:
 
     @staticmethod
     def to_ddmmyyHHMISS(date: datetime) -> str:
-        custom_format = "%d%m%y%H%M%S"
+        '''
+        To transform date in format ddmmyyHHMMSS
+
+        Parameters:
+        - date: date in datetime format
+        Returns:
+            str
+        '''
+        custom_format = '%d%m%y%H%M%S'
         return date.strftime(custom_format)
