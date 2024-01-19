058d55b1b123df88f571696fe193cbca9ec4c583
Aarteagaj <143422716+Aarteagaj@users.noreply.github.com>
2024-01-19 17:35:02 -0500

    read-files-and-concatenate (#13)
    * Se agrega metodo para lectura y concatenacion de archivos
    
    * Se agrega metodo move files
    
    * Se agrega documentacion de metodos
    
    * Se agrega documentacion de metodos

--- apollo11_simulator/models/report_processing/report_builder.py
+++ apollo11_simulator/models/report_processing/report_builder.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 from apollo11_simulator.models.report_processing.task_calculator import TaskCalculator
 
@@ -8,11 +9,38 @@ class ReportBuilder:
         self.__events: dict
 
     @classmethod
-    def from_path(cls, origin_path: Path) -> None:
-        pass
+    def from_path(cls, origin_path: str) -> str:
+        '''
+        To read files from path and merge content in str
 
-    def move_files(self, target_path: Path) -> None:
-        pass
-    
+        Parameters:
+        - origin_path: path where the files to read are located
+        Returns:
+            str
+        '''
+
+        list_files = os.listdir(origin_path)
+        content_merge = ''
+        for file in list_files:
+            with open(origin_path + '/' + file, "r") as content_file:
+                for line in content_file:
+                    if line != '':
+                        content_merge += line.strip() + "\n"
+            cls.move_files(origin_path, 'backup', file) # TODO: get target path from config file
+        return content_merge
+
+    def move_files(self, origin_path: str, target_path: str, filename: str) -> None:
+        '''
+        To read files from path and merge content in str
+
+        Parameters:
+        - origin_path: path where the files to read are located
+        - target_path: path where the file will be moved
+        - filename: file name
+        Returns:
+            None
+        '''
+        Path(origin_path + '/' + filename).rename(target_path + '/' + filename)
+
     def show_report():
         pass
